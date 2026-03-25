46664e9c408bbc55f3e60ce71f54e9343f6c4d1e
TirthDadhaniya <private.tirth@gmail.com>
2026-03-25 20:16:50 +0530

    server created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
