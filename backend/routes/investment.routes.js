f87d7668710d5f101e66248d54256bd7b2328a6e
TirthDadhaniya <private.tirth@gmail.com>
2026-03-29 16:15:27 +0530

    investment routes created

--- backend/routes/investment.routes.js
+++ backend/routes/investment.routes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const investmentController = require("../controller/investment.controller");
 
 router.post("/", investmentController.createInvestment);
+router.get("/", investmentController.getInvestment);
+router.get("/:id", investmentController.getInvestmentbyid);
+router.put("/:id", investmentController.updateInvestment);
+router.delete("/:id", investmentController.deleteInvestment);
 
 module.exports = router;
