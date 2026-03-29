528f6e93e9c013039e890a309b094c4999db7633
TirthDadhaniya <private.tirth@gmail.com>
2026-03-29 16:15:53 +0530

    sip routes created

---
+++ backend/routes/sip.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const sipController = require("../controller/sip.controller");
+
+router.get("/", sipController.getSIP);
+router.post("/", sipController.createSIP);
+router.put("/:id/status", sipController.updateStatus);
+router.put("/:id", sipController.updateSIP);
+router.delete("/:id", sipController.deleteSIP);
+
+module.exports = router;
