6aead74591908949693c3e6036c21641875b6ded
TirthDadhaniya <private.tirth@gmail.com>
2026-03-31 18:25:33 +0530

    folder renamed from config to db

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
-const connectDB = require("./config/db");
+const connectDB = require("./db/db");
 
 dotenv.config();
 
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// app.use("/api/portfolio", require("./routes/portfolio.routes"));
 app.use("/api/investments", require("./routes/investment.routes"));
 app.use("/api/sips", require("./routes/sip.routes"));
 app.use("/api/transactions", require("./routes/transaction.routes"));
