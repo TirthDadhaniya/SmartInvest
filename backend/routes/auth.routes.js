3e622fc43c177e7ae211680002ee2b34a31700bb
TirthDadhaniya <private.tirth@gmail.com>
2026-04-02 19:24:11 +0530

    routes protected by authMiddleware

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controller/auth.controller");
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
-router.get("/me", authController.getUser);
+router.get("/me", protect, authController.getUser);
+router.put("/me", protect, authController.updateUser);
 
 module.exports = router;
--- backend/routes/investment.routes.js
+++ backend/routes/investment.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const investmentController = require("../controller/investment.controller");
+const { protect } = require("../middleware/authMiddleware");
 
-router.post("/", investmentController.createInvestment);
-router.get("/", investmentController.getInvestments);
-router.get("/:id", investmentController.getInvestmentbyid);
-router.put("/:id", investmentController.updateInvestment);
-router.delete("/:id", investmentController.deleteInvestment);
+router.post("/", protect, investmentController.createInvestment);
+router.get("/", protect, investmentController.getInvestments);
+router.get("/:id", protect, investmentController.getInvestmentbyid);
+router.put("/:id", protect, investmentController.updateInvestment);
+router.delete("/:id", protect, investmentController.deleteInvestment);
 
 module.exports = router;
