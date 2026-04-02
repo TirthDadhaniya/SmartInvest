22df2d347f6d5454f0cd347a77e385c41d84b4f1
TirthDadhaniya <private.tirth@gmail.com>
2026-04-02 15:33:20 +0530

    authMiddleware created

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const Protext = async (req, res, next) => {
+  try {
+    let token;
+
+    // Check header
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    // If no token
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized",
+      });
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Get user from DB
+    const user = await User.findById(decoded.id).select("-passwordHash");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Attach user to request
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Not authorized, token failed",
+    });
+  }
+};
