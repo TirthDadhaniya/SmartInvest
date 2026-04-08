ebd6a3f6a13c0d52b799acdc2988ffd4c072bd3f
TirthDadhaniya <private.tirth@gmail.com>
2026-04-08 19:57:43 +0530

    mantual post route removed

--- backend/routes/transaction.routes.js
+++ backend/routes/transaction.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 
 const transactionController = require("../controller/transaction.controller");
 
-router.get("/", transactionController.getTransactions);
-router.post("/", transactionController.createTransaction);
+router.get("/", protect, transactionController.getTransactions);
+// router.post("/", protect, transactionController.createTransaction);
 
 module.exports = router;
