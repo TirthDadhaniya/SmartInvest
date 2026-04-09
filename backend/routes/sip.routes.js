ba155f64f29287ebb2081eb1b88fda1c669a2edb
TirthDadhaniya <private.tirth@gmail.com>
2026-04-09 20:15:56 +0530

    routes protected by middleware

--- backend/routes/sip.routes.js
+++ backend/routes/sip.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 
 const sipController = require("../controller/sip.controller");
 
-router.get("/", sipController.getSIPs);
-router.post("/", sipController.createSIP);
-router.put("/:id/status", sipController.updateStatus);
-router.put("/:id", sipController.updateSIP);
-router.delete("/:id", sipController.deleteSIP);
+router.get("/", protect, sipController.getAllSIPs);
+router.post("/", protect, sipController.createSIP);
+router.put("/:id/status", protect, sipController.updateSIPStatus);
+router.put("/:id", protect, sipController.updateSIP);
+router.delete("/:id", protect, sipController.deleteSIP);
 
 module.exports = router;
