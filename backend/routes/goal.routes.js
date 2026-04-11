789ab8070b92801ac4a6f739b02b38fd3e379f0c
TirthDadhaniya <private.tirth@gmail.com>
2026-04-11 14:33:09 +0530

    goal all routes added

--- backend/routes/goal.routes.js
+++ backend/routes/goal.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const goalController = require("../controller/goal.controller");
+const { protect } = require("../middleware/authMiddleware");
 
-router.get("/", goalController.getGoals);
-router.post("/", goalController.createGoal);
-router.put("/:id", goalController.updateGoal);
-router.delete("/:id", goalController.deleteGoal);
-router.get("/progress", goalController.getGoalProgress);
+router.get("/", protect, goalController.getGoals);
+router.post("/", protect, goalController.createGoal);
+router.put("/:id", protect, goalController.updateGoal);
+router.delete("/:id", protect, goalController.deleteGoal);
+router.get("/:id/progress", protect, goalController.getGoalProgress);
 
 module.exports = router;
