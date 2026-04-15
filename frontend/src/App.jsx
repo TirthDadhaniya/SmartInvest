6bb62f05b8a5d4be5f4f4b51824c89a7cc2fccc8
TirthDadhaniya <private.tirth@gmail.com>
2026-04-15 22:03:33 +0530

    created path for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Investment from "./pages/Investment";
+import ManageFunds from "./pages/ManageFunds";
+import Fund from "./pages/Fund";
+import Transactions from "./pages/Transactions";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<Register />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/investment" element={<Investment />} />
+        <Route path="/manageFunds" element={<ManageFunds />} />
+        <Route path="/fund" element={<Fund />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
