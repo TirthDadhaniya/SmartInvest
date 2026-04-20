88ff62a93ae1a8f32539d786357a289d7264108d
TirthDadhaniya <private.tirth@gmail.com>
2026-04-20 23:53:38 +0530

    All routes wrapped inside AppShell component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Investment from "./pages/Investment";
+import Investments from "./pages/Investments";
 import ManageFunds from "./pages/ManageFunds";
 import Fund from "./pages/Fund";
 import Transactions from "./pages/Transactions";
@@ -14,16 +14,16 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AppShell />} />
-        {/* <Route path="/" element={<Register />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/investment" element={<Investment />} />
-        <Route path="/manageFunds" element={<ManageFunds />} />
-        <Route path="/fund" element={<Fund />} />
-        <Route path="/transactions" element={<Transactions />} />
-        <Route path="/profile" element={<Profile />} /> */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<AppShell />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/investments" element={<Investments />} />
+          <Route path="/manageFunds" element={<ManageFunds />} />
+          <Route path="/fund" element={<Fund />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </Router>
   );
