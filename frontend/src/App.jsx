719ecc5de9feafd5f1233f585793719e7adabc41
TirthDadhaniya <private.tirth@gmail.com>
2026-04-21 17:09:55 +0530

    Created AuthContext and wrapped all routes under parent comp for auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -9,6 +9,7 @@ import Fund from "./pages/Fund";
 import Transactions from "./pages/Transactions";
 import Profile from "./pages/Profile";
 import AppShell from "./components/AppShell";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
@@ -16,13 +17,16 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<AppShell />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/investments" element={<Investments />} />
-          <Route path="/manageFunds" element={<ManageFunds />} />
-          <Route path="/fund" element={<Fund />} />
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/profile" element={<Profile />} />
+        
+        {/* Protected Routes mapped inside AppShell */}
+        <Route path="/" element={<ProtectedRoute><AppShell /></ProtectedRoute>}>
+          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="investments" element={<Investments />} />
+          <Route path="manageFunds" element={<ManageFunds />} />
+          <Route path="fund" element={<Fund />} />
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
       </Routes>
     </Router>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { PortfolioProvider } from "./context/PortfolioContext.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <PortfolioProvider>
+      <App />
+    </PortfolioProvider>
+  </AuthProvider>
+);
